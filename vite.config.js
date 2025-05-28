3b5ff313fa663010ad823387351bc44a3656c168
LuisJa23 <javierlink22@gmail.com>
2025-05-28 17:32:19 -0500

    Agregar archivo index.html con la estructura básica de la aplicación Finnancy

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/',            // desplegará desde la raíz del dominio/S3
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  build: {
-    outDir: 'dist'      // carpeta de salida para producción
-  }
 })
