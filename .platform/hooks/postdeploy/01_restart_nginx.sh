acba84217128c23d2b6b51d4c15df6ab9ebf7481
LuisJa23 <javierlink22@gmail.com>
2025-06-04 16:40:33 -0500

    feat: Eliminar configuraciones de Elastic Beanstalk y scripts de hooks

--- .platform/hooks/postdeploy/01_restart_nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "=== Iniciando postdeploy hook ==="
-
-# Reiniciar nginx para aplicar configuraciones
-sudo service nginx reload
-
-echo "=== Postdeploy hook completado exitosamente ==="
